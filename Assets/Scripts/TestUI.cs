f6c3dae34103f4daccfc8f26d90254c96f7c2873
Irisdurantoteldia <idurangonzalez10@gmail.com>
2023-10-23 10:16:57 +0200

    New Scene with slider and test ui script

---
+++ Assets/Scripts/TestUI.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class TestUI : MonoBehaviour
+{
+    public TMP_Text label;
+    // Start is called before the first frame update
+    void Start()
+    {
+        label.text = "Hola món";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
